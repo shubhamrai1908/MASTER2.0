85e7f240c07a952f739e783fb223b129aa046c35
shubham rai <shubhamrai1908@gmail.com>
2023-06-13 00:40:16 +0530

    Navigation menu added

--- app/src/main/java/shubham/latest/master20/DashboardActivity.java
+++ app/src/main/java/shubham/latest/master20/DashboardActivity.java
@@ -4,29 +4,148 @@ import static android.content.ContentValues.TAG;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class DashboardActivity extends AppCompatActivity {
+    NavigationView nav;
+    View headerView;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String uid,name,email,phone,link;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_dashboard);
-        try {
+        nav = findViewById(R.id.navmenu);
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            finish();
+        }
+        name = "Name";
+        email = "Email ID";
+        phone = "Phone Number";
+        link = "";
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.main);
+        nav = findViewById(R.id.navmenu);
+        headerView = nav.getHeaderView(0);
+        TextView Name = headerView.findViewById(R.id.name);
+        TextView Email = headerView.findViewById(R.id.email);
+        TextView Phone = headerView.findViewById(R.id.phone);
+        ImageView photo = headerView.findViewById(R.id.photo);
+        Name.setText(name);
+        Email.setText(email);
+        Phone.setText(phone);
+
+        drawerLayout = findViewById(R.id.drawer);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        Fragment fragment = new DashboardFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                int itemId = item.getItemId();
+                if (itemId == R.id.addground) {
+                    startActivity(new Intent(getApplicationContext(), AddGround.class));
+                    finish();
+                } else if (itemId == R.id.viewground) {
+                    startActivity(new Intent(getApplicationContext(), ViewGround.class));
+                    finish();
+                } else if (itemId == R.id.viewserver) {
+                    startActivity(new Intent(getApplicationContext(), ViewServer.class));
+                    finish();
+                } else if (itemId == R.id.addserver) {
+                    startActivity(new Intent(getApplicationContext(), AddServer.class));
+                    finish();
+                }
+                return true;
+
+            }
+        });
+
+
+        nav.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = new DashboardFragment();
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                String title = (String) item.getTitle();
+
+                if (title.equals("Pending Request")) {
+                    fragment = new FirstFragment();
+                    toolbar.setTitle("Pending Request");
+                } else if (title.equals("Ground Request")) {
+                    fragment = new SecondFragment();
+                    toolbar.setTitle("Ground Request");
+                } else if (title.equals("Server Request")) {
+                    fragment = new ThirdFragment();
+                    toolbar.setTitle("Server Request");
+                } else if (title.equals("Server Reply")) {
+                    fragment = new ForthFragment();
+                    toolbar.setTitle("Server Reply");
+                } else if (title.equals("Ground Reply")) {
+                    fragment = new FifthFragment();
+                    toolbar.setTitle("Ground Reply");
+                } else if (title.equals("Sign Out")) {
+                    Toast.makeText(getApplicationContext(),"logging out",Toast.LENGTH_LONG).show();
+                    logOut();
+                }
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
+            }
+        });
+
+    }
+    public void logOut()
+    {
+        Toast.makeText(getApplicationContext(),"logging out",Toast.LENGTH_LONG).show();
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        finish();
+
+    }}
+
+    /*    try {
             ActionBar actionBar = getSupportActionBar();
             actionBar.setTitle("Dashboard");
             actionBar.setDisplayUseLogoEnabled(true);
@@ -79,7 +198,7 @@ public class DashboardActivity extends AppCompatActivity {
                         try {
                             Ground ground = ds.getValue(Ground.class);
                             db.grounglistTable(ground.name,ground.phone);
-                             Toast.makeText(DashboardActivity.this,"inserted",Toast.LENGTH_SHORT).show();
+                          //   Toast.makeText(DashboardActivity.this,"inserted",Toast.LENGTH_SHORT).show();
 
 
                         }catch (Exception e)
@@ -101,40 +220,5 @@ public class DashboardActivity extends AppCompatActivity {
             Toast.makeText(DashboardActivity.this,e.toString(),Toast.LENGTH_LONG).show();
         }
     }
-    private void storeserver() {
 
-        try {
-
-            MasterDatabase db=new MasterDatabase(getApplicationContext());
-            DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("ground");
-            mDatabase.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    db.eraseData("groundlist_table");
-                    for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                        try {
-                            Ground ground = ds.getValue(Ground.class);
-                            db.grounglistTable(ground.name,ground.phone);
-                            Toast.makeText(DashboardActivity.this,"inserted",Toast.LENGTH_SHORT).show();
-
-
-                        }catch (Exception e)
-                        {
-                            Toast.makeText(DashboardActivity.this,e.toString(),Toast.LENGTH_LONG).show();
-                        }
-
-                    }
-                }
-
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                }
-            });
-
-        }catch (Exception e)
-        {
-            Toast.makeText(DashboardActivity.this,e.toString(),Toast.LENGTH_LONG).show();
-        }
-    }
-}
+}*/
---
+++ app/src/main/java/shubham/latest/master20/DashboardFragment.java
@@ -0,0 +1,99 @@
+package shubham.latest.master20;
+
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DashboardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DashboardFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DashboardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DashboardFragment newInstance(String param1, String param2) {
+        DashboardFragment fragment = new DashboardFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView= inflater.inflate(R.layout.fragment_dashboard, container, false);
+         //TextInputLayout textInputLayout= requireView().findViewById(R.id.menu);
+        final SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(this.getActivity());
+        TextView textView=rootView.findViewById(R.id.tv);
+        AutoCompleteTextView autoCompleteTextView= rootView.findViewById(R.id.serversim);
+        ArrayList<String> sim_cards=new ArrayList<>();
+        sim_cards.add("SIM-1");
+        sim_cards.add("SIM-2");
+        ArrayAdapter<String> stringArrayAdapter=new ArrayAdapter<>(getActivity(), android.R.layout.select_dialog_item,sim_cards);
+        autoCompleteTextView.setAdapter(stringArrayAdapter);
+        //autoCompleteTextView.setThreshold(1);
+        int pos = preferences.getInt("SelectedSim", -1);
+        if(pos>=0)
+            autoCompleteTextView.setText(sim_cards.get(pos),false);
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                preferences.edit().putInt("SelectedSim",position).apply();
+                Toast.makeText(getActivity(),sim_cards.get(position)+" is Selected as Default sim",Toast.LENGTH_SHORT).show();
+            }
+
+
+        });
+
+        return rootView;
+    }
+}
